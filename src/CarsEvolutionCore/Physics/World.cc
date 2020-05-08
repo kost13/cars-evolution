a8d4a4fc2b7973390e006b7bb3669d0e0b9894a8
Marcin Gajewski <mgajews2>
2020-05-09 00:50:36 +0200

    fix car generation, add settings structure

--- src/CarsEvolutionCore/Physics/World.cc
+++ src/CarsEvolutionCore/Physics/World.cc
@@ -28,11 +28,8 @@ cer::physics::World::World(const cer::CarsPopulationData &population,
     memset(&m_maxProfile, 0, sizeof(b2Profile));
     memset(&m_totalProfile, 0, sizeof(b2Profile));
 
-    m_hz = 4.0f;
-    m_zeta = 0.7f;
-    m_speed = 50.0f;
 
-            b2Body* ground = NULL;
+            b2Body* ground = nullptr;
             {
                     b2BodyDef bd;
                     ground = m_world->CreateBody(&bd);
@@ -41,15 +38,24 @@ cer::physics::World::World(const cer::CarsPopulationData &population,
 
                     b2FixtureDef fd;
                     fd.shape = &shape;
-                    fd.density = 0.0f;
-                    fd.friction = 0.6f;
+                    fd.density = settings.ground_density;
+                    fd.friction = settings.ground_friction;
 
 
-                    const unsigned int size=20;
+                    const int size=20;  //settings.number_of_stages;
+                    const float dx = settings.stage_width_x;
 
-                    shape.Set(b2Vec2(-20.0f, 0.0f), b2Vec2(20.0f, 0.0f));
+
+                    //start is flat, at small plate, size 2*dx
+                    shape.Set(b2Vec2(-2*dx, 0.0f), b2Vec2(2*dx, 0.0f));
                     ground->CreateFixture(&fd);
 
+                    float x = 2*dx;
+                    float y1 = 0.0f;
+
+
+                    //można potem zamienić na wektor, jak zdecydujemy
+                    //gdzie to przechowujemy
                     float hs[size] = {1.0f, 3.0f, 4.0f, -1.0f, -2.0f, 1.0f,
                                       2.0f, 5.0f, -1.25f, 0.0f,1.0f, 1.0f,
                                       4.0f, 0.0f, 0.0f, -1.0f, -2.0f, -5.0f,
@@ -58,7 +64,7 @@ cer::physics::World::World(const cer::CarsPopulationData &population,
 
                      // float hs[size];
 
-                    float x = 20.0f, y1 = 0.0f, dx = 10.0f;
+
 
                     for (unsigned int i = 0; i < size; ++i)
                     {
@@ -73,8 +79,8 @@ cer::physics::World::World(const cer::CarsPopulationData &population,
                     //wall at the end
                     //remove if track could be extended
 
-                    x += 20.0f;
-                    shape.Set(b2Vec2(x, 0.0f), b2Vec2(x, 20.0f));
+                    x += 2*dx;
+                    shape.Set(b2Vec2(x, 0.0f), b2Vec2(x, 2*dx));
                     ground->CreateFixture(&fd);
 
             }
@@ -82,151 +88,213 @@ cer::physics::World::World(const cer::CarsPopulationData &population,
 
 
 
-void cer::physics::World::generateCars(const cer::CarsPopulationData &population,
+std::vector<cer::physics::Car> cer::physics::World::generateCars(const cer::CarsPopulationData &population,
                                        cer::SimulationData *simulation_data){
 
 
-    //wpisać ilosc samochodów
-    int N=20;
-
-    //wpisać ilość punktów dla polygonu samochodu
-    //jeżeli jest zmienna to to uwzględnić niżej!
-    static const int BODY_POINTS_NUM = 8;
+    //number of cars in simulation
+    static const size_t car_No=population.cars().carsNum();
+    static const int BODY_POINTS_NUM = population.cars().parametersNum()-2;
 
     //vector of cars
-    std::vector<Car> cars(N);
+    std::vector<Car> cars;
 
-    static const int container_size=2*BODY_POINTS_NUM+2;
-    std::vector<double> carParameters(container_size);
-    std::vector<double>::iterator it = population.carsVector().begin();
+    std::vector<double>::iterator it;
 
 
     //for each car
-    for(int i=0;i<N;i++){
+    for(size_t i=0;i<car_No;i++){
 
 
-        //dobrze kopiuje?
+        Car car_t; //temporary object
 
-        copy(it, it+container_size, carParameters.begin());
-        it=it+container_size;
+        //radiuses of wheels are saved
+        it=population.cars().begin(i);
+        car_t.circle_front.m_radius = *it;
+        it++;
+        car_t.circle_rear.m_radius = *it;
+        it++;
+
+
+        //coordinates of wheels are set later
+        double wheel1_x=0;
+        double wheel1_y=0;
+
+        double wheel2_x=0;
+        double wheel2_y=0;
+
+
 
         //shape of a car
-        for( int j=2;j<BODY_POINTS_NUM;j++)
-            cars[i].vertices[j].Set(carParameters[j],carParameters[j+1] );
 
-        cars[i].chassis.Set(cars[i].vertices, BODY_POINTS_NUM);
+        int j=0;
+        for( it,j;it<population.cars().end(i) && j<BODY_POINTS_NUM;it+=2,j++){
+            car_t.vertices[j].Set(*it,*(it+1));
+
+            /*there is no method for getting coordinates from vertices
+            so it is better to save them*/
+            if(j==settings.wheel1_point_no){
+                wheel1_x=*it;
+                wheel1_y=*(it+1);
+            }
+
+            if(j==settings.wheel2_point_no){
+                wheel2_x=*it;
+                wheel2_y=*(it+1);
+            }
+
+
+        }
+
+        car_t.chassis.Set(car_t.vertices, BODY_POINTS_NUM);
+
 
 
 
         //jezeli koło nie bedzie sie krecic to ten blok skopiowac
         //do wheel 2
-        cars[i].circle.m_radius = carParameters[0];
-        cars[i].bd.type = b2_dynamicBody;
-        cars[i].bd.position.Set(0.0f, 1.0f);
-        m_car = m_world->CreateBody(&cars[i].bd);
 
-        m_car->CreateFixture(&cars[i].chassis, 1.0f);
-        cars[i].fd.shape = &cars[i].circle;
-        cars[i].fd.density = 1.0f;
-        cars[i].fd.friction = 0.9f;
+        car_t.bd.type = b2_dynamicBody;
+        car_t.bd.position.Set(0.0f, 1.0f);
+        m_car = m_world->CreateBody(&car_t.bd);
 
-
-        cars[i].bd.position.Set(-1.0f, 0.35f);  //pozycje kół
-        m_wheel1 = m_world->CreateBody(&cars[i].bd);
-        m_wheel1->CreateFixture(&cars[i].fd);
-
-
-        //jakby był problem z 2 kołem to sprawdzic ten blok
-        cars[i].circle2.m_radius = carParameters[0];
-
-        m_car->CreateFixture(&cars[i].chassis, 1.0f);
-        cars[i].fd.shape = &cars[i].circle2;
-        cars[i].fd.density = 1.0f;
-        cars[i].fd.friction = 0.9f;
-
-        cars[i].bd.position.Set(1.0f, 0.4f);    //pozycje kół
-        m_wheel2 = m_world->CreateBody(&cars[i].bd);
-        m_wheel2->CreateFixture(&cars[i].fd);
-        //
-
-
-
-        //wheel 1 parameters
         b2Vec2 axis(0.0f, 1.0f);
-        cars[i].jd.Initialize(m_car, m_wheel1, m_wheel1->GetPosition(), axis);
-        cars[i].jd.motorSpeed = 1.0f;
-        cars[i].jd.maxMotorTorque = 20.0f;
-        cars[i].jd.enableMotor = true;
-        cars[i].jd.frequencyHz = m_hz;
-        cars[i].jd.dampingRatio = m_zeta;
 
-        //wheel 2 parameters
-        m_spring1 = (b2WheelJoint*)m_world->CreateJoint(&cars[i].jd);
-        cars[i].jd.Initialize(m_car, m_wheel2, m_wheel2->GetPosition(), axis);
-        cars[i].jd.motorSpeed = 1.0f;
-        cars[i].jd.maxMotorTorque = 10.0f;
-        cars[i].jd.enableMotor = false;
-        cars[i].jd.frequencyHz = m_hz;
-        cars[i].jd.dampingRatio = m_zeta;
-        m_spring2 = (b2WheelJoint*)m_world->CreateJoint(&cars[i].jd);
 
+
+        //wheel 1
+        m_car->CreateFixture(&car_t.chassis, 1.0f);
+        car_t.fd.shape = &car_t.circle_front;
+        car_t.fd.density = settings.wheel1_density;
+        car_t.fd.friction = settings.wheel1_friction;
+
+        car_t.bd.position.Set(wheel1_x, wheel1_y);  //wheel position
+        m_wheel1 = m_world->CreateBody(&car_t.bd);
+        m_wheel1->CreateFixture(&car_t.fd);
+
+        car_t.jd.Initialize(m_car, m_wheel1, m_wheel1->GetPosition(), axis);
+        car_t.jd.motorSpeed = settings.motor1_speed;
+        car_t.jd.maxMotorTorque = settings.motor1_maxTorque;
+        car_t.jd.enableMotor = settings.motor1_enable;
+        car_t.jd.frequencyHz = settings.motor1_frequencyHz;
+        car_t.jd.dampingRatio = settings.wheel1_dampingRatio;
+        m_spring1 = (b2WheelJoint*)m_world->CreateJoint(&car_t.jd);
+
+
+
+        //wheel 2
+        m_car->CreateFixture(&car_t.chassis, 1.0f);
+        car_t.fd.shape = &car_t.circle_rear;
+        car_t.fd.density = settings.wheel2_density;
+        car_t.fd.friction = settings.wheel2_friction;
+
+        car_t.bd.position.Set(wheel2_x, wheel2_y);    //wheel position
+        m_wheel2 = m_world->CreateBody(&car_t.bd);
+        m_wheel2->CreateFixture(&car_t.fd);
+
+        car_t.jd.Initialize(m_car, m_wheel2, m_wheel2->GetPosition(), axis);
+        car_t.jd.motorSpeed = settings.motor2_speed;
+        car_t.jd.maxMotorTorque = settings.motor2_maxTorque;
+        car_t.jd.enableMotor = settings.motor2_enable;
+        car_t.jd.frequencyHz = settings.motor2_frequencyHz;
+        car_t.jd.dampingRatio = settings.wheel2_dampingRatio;
+        m_spring2 = (b2WheelJoint*)m_world->CreateJoint(&car_t.jd);
+
+
+        cars.push_back(car_t);
     }
+
+    return cars;
 
 }
 
 
 
 
-bool cer::physics::World::runSimulation() {
-  auto cars_num = population_.cars().carsNum();
-  simulation_data_->reset(cars_num);
+bool cer::physics::World::runSimulation(std::vector<cer::physics::Car> cars) {
+
+    /* for dummy simulation
+    auto cars_num = population_.cars().carsNum();
+    simulation_data_->reset(cars_num);
+    */
+
+    /*
+    // dummy simulation
+    std::vector<float> speeds(cars_num);
+    for (auto &s : speeds) {
+      s = float(rand() % 10);
+    }
+    */
 
 
-// #########################################################
-
-//chyba wyrzucić
 #if defined(_WIN32)
-    // Enable memory-leak reports
-    _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG));
+// Enable memory-leak reports
+_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG));
 #endif
 
 
-//s_settings.Load();
+  float timeStep = settings.m_hertz > 0.0f ? 1.0f / settings.m_hertz : float(0.0f);
+
+  //m_world->SetAllowSleeping(settings.m_enableSleep);
+  m_world->SetWarmStarting(settings.m_enableWarmStarting);
+  m_world->SetContinuousPhysics(settings.m_enableContinuous);
+  m_world->SetSubStepping(settings.m_enableSubStepping);
 
 
 
-  //dałbym funkcję inititate z kosntruktorem
+  bool stop=0; //flag for simulation control
 
-  //w pętli funkcję step
-
-  //ustawienia w settingsach
-
-  // dummy simulation
-  std::vector<float> speeds(cars_num);
-  for (auto &s : speeds) {
-    s = float(rand() % 10);
-  }
-
-
-
-  for (int j = 0; j < 100; ++j) {
+  while(!stop) {
     // simulate computations
     //    logger::info() << "runSimulation" << j;
-    {
+
+
+
       for (size_t i = 0; i < cars_num; ++i) {
 
+          b2Vec2 position = &cars[i].bd->GetPosition();
+          float angle = &cars[i].bd->GetAngle();
 
-          //pętla
-          //s_test->Step(s_settings);
+          m_world->Step(timeStep, settings.m_velocityIterations, settings.m_positionIterations);
 
 
+          // Now print the position and angle of the body.
+          position = body->GetPosition();
+          angle = body->GetAngle();
+
+
+
+        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
         simulation_data_->pushPosition(
             i, Position{(1.5f * speeds[i] + 1.0f) * j,
                         (1.0f + 0.5f * speeds[i]) * j, speeds[i] * 0.1f * j});
-      }
+
+
+        int k=0;
+        if(true /*pozycja sie nie zmienia*/)
+            k++;
+        else
+            k=0;
+
+        int max_iter=10000;
+
+
+        bool flag=0;
+        if(k>max_iter)  //flaga up
+            flag=1;
+
+
+
+
+
     }
   }
+
+
   return true;
 }
+
+
+
 
 float cer::physics::World::vecNorm() const { return vec_.Length(); }
--- src/CarsEvolutionCore/Physics/World.h
+++ src/CarsEvolutionCore/Physics/World.h
@@ -18,6 +18,7 @@
 #include <crtdbg.h>
 #endif
 
+#include "ParametersMatrix.h"
 struct Settings;
 
 
@@ -70,8 +71,8 @@ struct Car {
 
   b2PolygonShape chassis;
   b2Vec2 vertices[BODY_POINTS_NUM];
-  b2CircleShape circle;
-  b2CircleShape circle2;
+  b2CircleShape circle_front;
+  b2CircleShape circle_rear;
   b2BodyDef bd;
   b2FixtureDef fd;
   b2WheelJointDef jd;
@@ -82,17 +83,73 @@ struct Car {
 
 //##################################
 
+
+
+struct Settings{
+
+    //simualation and world
+    float m_hertz= 60.0f;
+    int m_velocityIterations=8;
+    int m_positionIterations=3;
+    bool m_enableWarmStarting=true;
+    bool m_enableContinuous=true;
+    bool m_enableSubStepping=false;
+
+
+    //scene
+    float ground_friction=0.6f;
+    float ground_density=0.0f; //recommended!
+    int number_of_stages=20;   /* jak zdecydujemy się na jakiś sposób
+                                przechowywania to zmienić w cpp przypisanie
+                                chwilowo jest stała macierz    */
+    float stage_width_x=10.0f;
+
+
+
+    //cars
+
+
+        //wheel1
+        int wheel1_point_no=1; //to which point of body, wheel1 is attached
+        float wheel1_density=1.0f;
+        float wheel1_friction=0.9f;
+        float motor1_speed=1.0f;
+        float motor1_maxTorque=20.0f;
+        bool  motor1_enable=true;
+        float motor1_frequencyHz=4.0f;
+        float wheel1_dampingRatio=0.7f;
+
+
+
+        //wheel2
+        int wheel2_point_no=4; //to which point of body, wheel2 is attached
+        float wheel2_density=1.0f;
+        float wheel2_friction=0.9f;
+        float motor2_speed=1.0f;
+        float motor2_maxTorque=20.0f;
+        bool  motor2_enable=true;
+        float motor2_frequencyHz=4.0f;
+        float wheel2_dampingRatio=0.7f;
+
+
+
+
+
+
+
+};
+
 class World {
  public:
   explicit World(const CarsPopulationData &population,
                  SimulationData *simulation_data);
   ~World()=default;
 
-  bool runSimulation();
+  bool runSimulation(std::vector<cer::physics::Car> cars);
 
   void Step(/*Settings& settings*/);
   void ShiftOrigin(const b2Vec2& newOrigin);
-  void generateCars(const cer::CarsPopulationData &population,
+  std::vector<cer::physics::Car> generateCars(const cer::CarsPopulationData &population,
                                          cer::SimulationData *simulation_data);
 
 
@@ -121,12 +178,11 @@ class World {
   b2Body* m_wheel1;
   b2Body* m_wheel2;
 
-  float m_hz;
-  float m_zeta;
-  float m_speed;
+
   b2WheelJoint* m_spring1;
   b2WheelJoint* m_spring2;
 
+  Settings settings;
   //###################################
 
 
