606bba361708279e0f2eee2cb435c499d35f5d77
lukasz <l.kostrzewa12@gmail.com>
2020-05-31 23:58:21 +0200

    add core and evolution tests

--- tests/CarsEvolutionCoreTests.cc
+++ tests/CarsEvolutionCoreTests.cc
@@ -7,52 +7,56 @@
 #include "CarsPopulationData.h"
 #include "SimulationData.h"
 
-BOOST_AUTO_TEST_CASE(population_test) {
-  using cer::CarParameters;
-  auto population = std::vector<CarParameters>{
-      CarParameters{1., 2., std::vector<double>{3., 4., 5., 6.}},
-      CarParameters{7., 8., std::vector<double>{9., 10., 11., 12.}}};
-  cer::CarsPopulationData o;
-  o.setCars(population);
+BOOST_AUTO_TEST_CASE(population_parameters_test) {
+  using cer::CarsPopulationData;
 
-  BOOST_TEST(o.cars().size() == 2);
-  BOOST_TEST(o.carsVector().size() == 2 * 6);
+  BOOST_CHECK_THROW(cer::ParametersMatrix(std::vector<double>{1, 2, 3}),
+                    std::runtime_error);
 
-  BOOST_TEST(o.car(0).front_wheel == 1.);
-  BOOST_TEST(o.car(0).rear_wheel == 2.);
-  BOOST_TEST(o.car(0).body_points.size() == 4);
-  BOOST_TEST(o.car(0).body_points[1] == 4.);
+  cer::ParametersMatrix mat;
+  int cars_num = 6;
+  std::vector<double> params(cer::ParametersMatrix::parametersNum() * cars_num,
+                             1.0);
+  BOOST_CHECK_NO_THROW(mat.setParameters(params));
 
-  BOOST_TEST(o.car(1).front_wheel == 7.);
-  BOOST_TEST(o.car(1).rear_wheel == 8.);
-  BOOST_TEST(o.car(1).body_points.size() == 4);
-  BOOST_TEST(o.car(1).body_points[3] == 12.);
+  cer::CarsPopulationData d;
+  BOOST_CHECK(d.empty());
+  d.setCars(mat);
+  BOOST_CHECK(!d.empty());
+  BOOST_CHECK(!d.cars().empty());
 
-  o.setCars({});
-  BOOST_TEST(o.cars().size() == 0);
-  BOOST_TEST(o.carsVector().size() == 2 * 0);
+  BOOST_CHECK_EQUAL(d.cars().carsNum(), cars_num);
+  BOOST_CHECK_EQUAL(d.cars().size(),
+                    cer::ParametersMatrix::parametersNum() * cars_num);
+
+  BOOST_CHECK_EQUAL(std::distance(mat.begin(0), mat.end(0)),
+                    cer::ParametersMatrix::parametersNum());
+
+  BOOST_CHECK_EQUAL(std::distance(mat.cbegin(1), mat.cend(1)),
+                    cer::ParametersMatrix::parametersNum());
+
+  for (auto it = mat.begin(0); it != mat.end(0); ++it) {
+    BOOST_CHECK_EQUAL(*it, 1.0);
+  }
+
+  std::vector<double> new_params(
+      cer::ParametersMatrix::parametersNum() * cars_num, 2.0);
+  BOOST_CHECK_NO_THROW(mat.setParameters(new_params));
+
+  for (auto it = mat.begin(1); it != mat.end(1); ++it) {
+    BOOST_CHECK_EQUAL(*it, 2.0);
+  }
+
+  BOOST_CHECK_NO_THROW(mat.setCarParameters(
+      2, std::vector<double>(cer::ParametersMatrix::parametersNum(), 3.0)));
+
+  for (auto it = mat.begin(3); it != mat.end(3); ++it) {
+    BOOST_CHECK_EQUAL(*it, 2.0);
+  }
+
+  for (auto it = mat.begin(2); it != mat.end(2); ++it) {
+    BOOST_CHECK_EQUAL(*it, 3.0);
+  }
 }
 
-BOOST_AUTO_TEST_CASE(simulation_data_test) {
-  using cer::Position;
-  cer::SimulationData o;
-  BOOST_TEST(o.carsNum() == 0);
-  o.reset(2);
-  BOOST_TEST(o.popPosition(0).x == -1);
-  BOOST_TEST(o.carsNum() == 2);
-  BOOST_CHECK_THROW(o.popPosition(2), std::out_of_range);
-
-  o.pushPosition(0, {1, 1, 1});
-  o.pushPosition(0, {2, 2, 2});
-  o.pushPosition(1, {2, 3, 4});
-  BOOST_CHECK_THROW(o.pushPosition(3, {3, 3, 3}), std::out_of_range);
-
-  BOOST_TEST(o.popPosition(0).y == 1);
-  BOOST_TEST(o.popPosition(0).theta == 2);
-  BOOST_TEST(o.popPosition(0).x == -1);
-
-  auto pos = o.popPosition(1);
-  BOOST_TEST(pos.x == 2);
-  BOOST_TEST(pos.y == 3);
-  BOOST_TEST(pos.theta == 4);
-}
+BOOST_AUTO_TEST_CASE(simulation_data_test) {}
