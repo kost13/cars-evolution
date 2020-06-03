edc7cfd376c306cd5ae009d326102cce56761556
lukasz <l.kostrzewa12@gmail.com>
2020-06-03 22:32:05 +0200

    fix physics tests

--- tests/PhysicsTests.cc
+++ tests/PhysicsTests.cc
@@ -15,11 +15,10 @@ BOOST_AUTO_TEST_CASE(world_test) {
   cer::evolution::Evolution ev(&pd, 12345);
   ev.generatePopulation();
   auto population = pd.cars();
-  ev.setPopulationFitness(std::vector<double>(population.carsNum(), 1.0));
-  ev.generatePopulation();
   auto params = ev.parameters();
-  cer::SimulationData *simulation_data_;
-  cer::physics::World world(pd, simulation_data_);
+  cer::SimulationData simulation_data;
+  simulation_data.reset(population.carsNum());
+  cer::physics::World world(pd, &simulation_data);
   std::vector<std::pair<double, double>> route;
   float dx = 2.0f;
   route.emplace_back(-5 * dx, 5 * dx);
@@ -42,8 +41,7 @@ BOOST_AUTO_TEST_CASE(world_test) {
       1.0f, 0.3f,  0.4f, -1.0f, -0.2f, 0.5f,  0.4f,  0.5f,  -0.25f, 0.0f,
       0.1f, -0.1f, 0.3f, 0.0f,  0.0f,  -0.1f, -0.2f, -0.5f, -0.25f, 0.0f};
 
-  for (unsigned int i = 0; i < size; ++i) {
-    float y2 = hs[i];
+  for (float y2 : hs) {
     y1 = y2;
     x += dx;
     route.emplace_back(x, y1);
@@ -55,10 +53,13 @@ BOOST_AUTO_TEST_CASE(world_test) {
 
   BOOST_CHECK(world.runSimulation());
 
-  bool flag = 0;
+  bool flag = false;
   std::vector<double> distances = world.maxDistanceReached();
-  for (const auto &p : distances)
-    if (p > 50) flag = 1;
+  for (const auto &p : distances) {
+    if (p > 50) {
+      flag = true;
+    }
+  }
 
   BOOST_CHECK(flag);
 }
