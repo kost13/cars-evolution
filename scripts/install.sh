f6a8b073fc826a29af6875c7eaf754507d3cce36
kost13 <l.kostrzewa12@gmail.com>
2020-06-04 17:10:59 +0000

    add qml modules needed in qt 12

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,10 @@
 
 sudo add-apt-repository universe
 sudo apt-get update
-sudo apt-get upgrade
+#sudo apt-get upgrade
 
 sudo apt-get install build-essential
 sudo apt-get install cmake
 sudo apt-get install libboost-test-dev
 sudo apt-get install qt5-default qtdeclarative5-dev libqt5charts5-dev qtquickcontrols2-5-dev
-sudo apt-get install qml-module-qtquick2 qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-dialogs qml-module-qtcharts
+sudo apt-get install qml-module-qtquick2 qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-dialogs qml-module-qtcharts qml-module-qt-labs-settings qml-module-qt-labs-folderlistmodel
